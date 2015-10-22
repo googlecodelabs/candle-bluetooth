25573f95e2ff3dd16c082ca8383dd2c8b5d2a74c
Francois Beaufort <beaufort.francois@gmail.com>
2015-10-22 16:33:27 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+document.querySelector('#connect').addEventListener('click', function(event) {
+  /* PLACEHOLDER */
+});
+
+function changeColor() {
+  /* PLACEHOLDER */
+}
+
+var r = g = b = 255;
+
+function onColorChanged(rgb) {
+  if (rgb) {
+    console.log('Color changed to ' + rgb);
+    r = rgb[0]; g = rgb[1]; b = rgb[2];
+  } else {
+    console.log('Color changed');
+  }
+}
+
+var img = new Image();
+img.src = 'color-wheel.png';
+img.onload = function() {
+  var canvas = document.querySelector('canvas');
+  var context = canvas.getContext('2d');
+
+  canvas.width = 300 * devicePixelRatio;
+  canvas.height = 300 * devicePixelRatio;
+  canvas.style.width = "300px";
+  canvas.style.height = "300px";
+  canvas.addEventListener('click', function(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var x = (evt.clientX - rect.left) * devicePixelRatio;
+    var y = (evt.clientY - rect.top) * devicePixelRatio;
+    var data = context.getImageData(0, 0, canvas.width, canvas.height).data;
+
+    r = data[((canvas.width * y) + x) * 4];
+    g = data[((canvas.width * y) + x) * 4 + 1];
+    b = data[((canvas.width * y) + x) * 4 + 2];
+
+    changeColor();
+  });
+
+  context.drawImage(img, 0, 0, canvas.width, canvas.height);
+}
+
+document.querySelector('#noEffect').addEventListener('click', changeColor);
+document.querySelector('#candleEffect').addEventListener('click', changeColor);
+document.querySelector('#flashing').addEventListener('click', changeColor);
+document.querySelector('#pulse').addEventListener('click', changeColor);
+document.querySelector('#rainbow').addEventListener('click', changeColor);
+document.querySelector('#rainbowFade').addEventListener('click', changeColor);
